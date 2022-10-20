4fea626a14c9d2951330b3329f0582895c780d1c
Buvude <90856761+Buvude@users.noreply.github.com>
2022-10-20 09:01:47 -0700

    added two players

--- MolassesFactoryHavoc/Assets/Scripts/PCSpriteController.cs
+++ MolassesFactoryHavoc/Assets/Scripts/PCSpriteController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteController : MonoBehaviour
+public class PCSpriteController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
